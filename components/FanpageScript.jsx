7d7ccc6d256e2a003448f0a918901bc58639f365
QuyDX <wolflavamc@gmail.com>
2023-01-26 16:41:44 +0700

    change bg

--- components/FanpageScript.jsx
+++ components/FanpageScript.jsx
@@ -7,7 +7,7 @@ const FanpageScript = () => {
        <Script strategy='lazyOnload'>
             {`
                 <div id="fb-root"></div>
-                <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v15.0" nonce="o2064nJ1"></script>
+                <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v15.0" nonce="Xfawv0Lj"></script>
             `}
         </Script>
     </>
