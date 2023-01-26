5091b63cb34860da1f0689a27b14479692477ab7
QuyDX <wolflavamc@gmail.com>
2023-01-26 11:58:05 +0700

    change head

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
             <div className='md:float-left block'>
                 <Link href='/'>
                     <span className="cursor-pointer font-bold text-4xl text-white drop-shadow-md hover:drop-shadow-lg transition-all">
-                        UniTech Blog
+                        UniTechBlog
                     </span>
                 </Link>
             </div>
