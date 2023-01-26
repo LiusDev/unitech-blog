e81611d194e9d0ecf197f06e068281a375e04de9
QuyDX <wolflavamc@gmail.com>
2023-01-26 17:03:38 +0700

    add gap

--- pages/index.jsx
+++ pages/index.jsx
@@ -24,7 +24,7 @@ export default function Home({ posts }) {
         <FanpageScript />
       </Head>
       <FeaturedPosts />
-      <div className="grid grid-cols-1 lg:grid-cols-12">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post, index) => (
             <PostCard key={index} post={post.node} />
