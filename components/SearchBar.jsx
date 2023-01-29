fc637484e4408a54bc3bc9f62aaac1bd18bc02bb
nguyenqtrang1409@gmail.com <nguyenqtrang1409@gmail.com>
2023-01-30 01:04:24 +0700

    create search bar UI

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const SearchBar = () => {
     // Todo: Search bar
     return (
+        // create a search bar UI using tailwind css
         <div>
-            hello
+            <input type="text" placeholder="Search" className="border border-sky-900 w-full h-10 px-5 pr-10 text-sm text-gray-700 placeholder-gray-400 bg rounded-full shadow focus:outline-none focus:shadow-outline" />
         </div>
     )
 }
---
+++ pages/nlqtrang.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button } from '../components'
+
+const nlqtrang = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center flex-col">
+        nlqtrang
+        <div>
+            <Button href="/">
+                Trang chủ
+            </Button>
+        </div>
+        
+    </div>
+  )
+}
+
+export default nlqtrang
