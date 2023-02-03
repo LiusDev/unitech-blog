db61589993e4e3e572a9c08aadb53766c7c1a227
QuyDX <wolflavamc@gmail.com>
2023-02-03 15:30:50 +0700

    feat: add messenger

--- components/MetaMessenger.jsx
+++ components/MetaMessenger.jsx
@@ -10,23 +10,23 @@ const MetaMessenger = () => {
             <Script strategy="lazyOnload">
                 { `
                     var chatbox = document.getElementById('fb-customer-chat');
-                    chatbox.setAttribute("page_id", "107446722251445");
+                    chatbox.setAttribute("page_id", "112610738407250");
                     chatbox.setAttribute("attribution", "biz_inbox");
 
                     window.fbAsyncInit = function() {
                         FB.init({
-                        xfbml            : true,
-                        version          : 'v15.0'
+                          xfbml            : true,
+                          version          : 'v16.0'
                         });
-                    };
-
-                    (function(d, s, id) {
+                      };
+                
+                      (function(d, s, id) {
                         var js, fjs = d.getElementsByTagName(s)[0];
                         if (d.getElementById(id)) return;
                         js = d.createElement(s); js.id = id;
                         js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
                         fjs.parentNode.insertBefore(js, fjs);
-                    }(document, 'script', 'facebook-jssdk'));
+                      }(document, 'script', 'facebook-jssdk'));
                 ` }
             </Script>
         </div>
