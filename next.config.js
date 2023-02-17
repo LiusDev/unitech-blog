6dc540a7cb3641dbe31f2d0eb2b17f27690dbc01
QuyDX <wolflavamc@gmail.com>
2023-02-17 15:09:50 +0700

    feat: add admin and analytics redirect page

--- next.config.js
+++ next.config.js
@@ -18,6 +18,16 @@ module.exports = {
         destination: 'https://www.facebook.com/SSG.UniTech.Blog',
         permanent: true,
       },
+      {
+        source: '/admin',
+        destination: 'https://app.hygraph.com/da39084b3a874a6a8304129c79c0d670/master/content/c5f12e97d02440c9a483bc8328658f0b/view/1554afcd2e59455cafdc835eaf401cde',
+        permanent: true,
+      },
+      {
+        source: '/analytics', 
+        destination: 'https://vercel.com/liusdev/unitech-blog/analytics/audience?period=90d',
+        permanent: true,
+      },
     ]
   },
 }
