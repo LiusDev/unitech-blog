d0af83b0cd66a300442c3bbee98f20fd2e11f796
QuyDX <wolflavamc@gmail.com>
2023-02-17 15:04:45 +0700

    feat: add fanpage redirect

--- next.config.js
+++ next.config.js
@@ -11,4 +11,13 @@ module.exports = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/fanpage',
+        destination: 'https://www.facebook.com/SSG.UniTech.Blog',
+        permanent: true,
+      },
+    ]
+  },
 }
