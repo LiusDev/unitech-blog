893e61ff128e776e58ec11aac20a681347c7acbd
QuyDX <wolflavamc@gmail.com>
2023-02-20 13:25:42 +0700

    feat: add google meta tag for all pages

--- pages/intro.jsx
+++ pages/intro.jsx
@@ -7,6 +7,8 @@ const intro = () => {
         <div className='container mx-auto px-5 md:px-10 mb-8 mt-20 min-h-screen'>
             <Head>
                 <title>Giới thiệu dự án UniTech Blog</title>
+                <meta property="og:image" content="/ogImg.png" />
+                <meta name="google-site-verification" content="3efLReZ1VSvaHUpupLzLSTBLOUkxKVlLrOiHKiaA_wQ" />
             </Head>
             <div className='grid grid-cols-1 lg:grid-cols-12 lg:gap-12'>
                 <div className='col-span-1 lg:col-span-8'>
