278ce910067d72e7b48429ae25c314191b80e4e0
QuyDX <wolflavamc@gmail.com>
2023-03-03 18:54:17 +0700

    feat: add empty post category

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -103,7 +103,7 @@ export default CategoryPost;
 
 // Fetch data at build time
 export async function getStaticProps({ params }) {
-  const posts = await getCategoryPost(params.slug);
+  const posts = await (getCategoryPost(params.slug)) || [];
 
   return {
     props: { posts },
